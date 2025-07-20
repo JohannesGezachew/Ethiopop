49aa658ba25cc17c412d427ee3bf297559ad25d3
Johannes <yohannis995@gmail.com>
2025-07-20 09:00:00 +0300

    Update development server port configuration and finalize project setup

--- webpack.config.js
+++ webpack.config.js
@@ -94,7 +94,7 @@ module.exports = (env, argv) => {
         directory: path.join(__dirname, 'public'),
       },
       compress: true,
-      port: 3000,
+      port: 3001,
       hot: true,
       historyApiFallback: true,
       open: true,
